821f9a95fe5fb180e4c6fc50aaf8eb05b3618469
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2023-05-12 21:38:29 -0300

    Clase20

---
+++ Clase19/js/funciones.js
@@ -0,0 +1,5 @@
+function funcion()
+{
+    x=document.getElementById("seleccion").value;
+    document.getElementById("suEleccion").innerHTML="Su Lenguaje es: "+x;
+}
---
+++ Clase20/js/funciones.js
@@ -0,0 +1,16 @@
+function cambiar(fondo,letra)
+{    
+    document.getElementById("texto").style.backgroundColor=fondo;
+    document.getElementById("texto").style.color=letra;   
+}
+
+function publicidad()
+{
+    alert("Muestro una publicidad");
+}
+
+function cambiarFondo(fondo)
+{   
+    document.getElementById("textoInput").style.backgroundColor=fondo;
+}
+
